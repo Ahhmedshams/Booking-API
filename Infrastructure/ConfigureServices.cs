27d7e81fe742ab6ffe9b0ab93b6e9c53bc239011
Ahhmedshams <ahmedtaha757@gmail.com>
2023-05-22 21:41:59 +0300

    init

---
+++ Infrastructure/ConfigureServices.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class ConfigureServices
+    {
+        //This is Extension function we use to configure Infrastructure Services instead of write it in program.cs
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("Connection1"),
+                      b => b.MigrationsAssembly("Infrastructure"));
+            });
+            return services;
+        }
+    }
+}
---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() { }
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+         
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
