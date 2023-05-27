b8702f7e1744a8e993824c58c5d185d734720929
ahmed-elagezy <ahmedelagezy0@gmail.com>
2023-05-27 22:09:25 +0300

    shedule

---
+++ src/Infrastructure/Persistence/Configurations/ScheduleConfigurations.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class ScheduleConfigurations : IEntityTypeConfiguration<Schedule>
+    {
+        public void Configure(EntityTypeBuilder<Schedule> builder)
+        {
+            builder.HasKey(i => new { i.ScheduleID, i.ResourceId });
+
+            builder.HasMany(s => s.ScheduleItem).WithOne(d => d.ScheduleData);
+
+            /*builder.HasMany<ScheduleItem>(s => s.ScheduleID)
+                .WithOne(s => s.ScheduleID);*/
+               
+
+            builder.Property(x => x.FromDate).HasColumnType("DATE").IsRequired();
+            builder.Property(x => x.ToDate).HasColumnType("DATE").IsRequired();
+
+        }
+    }
+}
